18103289f9cf0136b55e60399478ec56d2d3e661
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-08-18 15:16:04 +0300

    --

--- src/reservation/reservation.controller.ts
+++ src/reservation/reservation.controller.ts
@@ -2,33 +2,36 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ReservationService } from './reservation.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('reservation')
 
 @Controller('reservation')
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
 
-  @Post()
-  create(@Body() createReservationDto: CreateReservationDto) {
-    return this.reservationService.create(createReservationDto);
-  }
+  // @Post()
+  // create(@Body() createReservationDto: CreateReservationDto) {
+  //   return this.reservationService.create(createReservationDto);
+  // }
 
-  @Get()
-  findAll() {
-    return this.reservationService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.reservationService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reservationService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.reservationService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReservationDto: UpdateReservationDto) {
-    return this.reservationService.update(+id, updateReservationDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateReservationDto: UpdateReservationDto) {
+  //   return this.reservationService.update(+id, updateReservationDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reservationService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.reservationService.remove(+id);
+  // }
 }
